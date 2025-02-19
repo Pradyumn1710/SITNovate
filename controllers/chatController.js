db12de28e488b6b9986d816dba842c4d572bda96
mini thanvi <code.thanvi@gmail.com>
2025-02-20 01:48:32 +0530

    added chat functionality

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,26 +1,31 @@
-const ChatMessage = require('../models/Chat');
+const axios = require("axios");
+const ChatMessage = require("../models/Chat");
 
 exports.sendMessage = async (req, res) => {
   const { message } = req.body;
 
   try {
+    
     const userMessage = new ChatMessage({
       userId: req.user._id,
       message,
-      senderType: 'user'
+      senderType: "user",
     });
     await userMessage.save();
 
-    
+   
+    const aiResponse = await axios.post("http://127.0.0.1:5000/ask", { question: message });
+
+   
     const agentResponse = new ChatMessage({
       userId: req.user._id,
-      message: `Agent response to "${message}"`,
-      senderType: 'agent'
+      message: aiResponse.data.answer, 
+      senderType: "agent",
     });
     await agentResponse.save();
 
     res.json({ userMessage, agentResponse });
   } catch (error) {
-    res.status(500).json({ message: 'Error sending message', error });
+    res.status(500).json({ message: "Error processing message", error: error.message });
   }
 };
